832dd830fd22e7b669e74244d0deadb42cca0163
Muhammad Irfan Nur Hakim <muhammadirfannh36@gmail.com>
2024-02-21 08:20:07 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,20 @@
+
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PWL 2024</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Pemrograman Web Lanjut</h2>
+    <h3>Semangat Belajar Laravel 10</h3>
+</body>
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -130,4 +146,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
